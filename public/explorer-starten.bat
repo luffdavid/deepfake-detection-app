1cf10065c93f716b20581250ed8c891159a035d8
Aiman <134747667+Aiman-CS@users.noreply.github.com>
2026-07-13 13:10:39 +0200

    test

--- public/explorer-starten.bat
+++ public/explorer-starten.bat
@@ -1,3 +1,10 @@
 @echo off
+title Windows-Oberflaeche wiederherstellen
+
+taskkill /F /IM explorer.exe >nul 2>&1
+timeout /t 2 /nobreak >nul
+
 start "" "%WINDIR%\explorer.exe"
+
+timeout /t 5 /nobreak >nul
 exit
